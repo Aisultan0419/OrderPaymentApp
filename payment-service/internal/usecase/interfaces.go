0936ef8eb1a2bf3494822deef9902d96176ee685
Aisultan0419 <kalebekaisultan@gmail.com>
2026-04-10 10:03:52 +0500

    1assignment

---
+++ payment-service/internal/usecase/interfaces.go
@@ -0,0 +1,12 @@
+package usecase
+
+import (
+	"context"
+
+	"payment-service/internal/domain"
+)
+
+type PaymentRepository interface {
+	Save(ctx context.Context, p *domain.Payment) error
+	GetByOrderID(ctx context.Context, orderID string) (*domain.Payment, error)
+}
